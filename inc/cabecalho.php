2af5ba06520c0853ac0fb112d9e746418b8e2d9a
kaue-S <kauesilva38@hotmail.com>
2023-11-12 17:03:02 -0300

    criação do menu

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -1,15 +1,26 @@
-<?php
-    
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Página inicial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Cabeçalho</title>
 </head>
 <body>
-    <h1></h1>
+    <header>
+        <nav>
+            <span>Salvapets</span>
+            <ul>
+                <li>Cães</li>
+                <li>Gatos</li>
+                <li>Ajuda</li>
+                <li>Quem somos</li>
+                <li>Contato</li>
+            </ul>
+            <button>Entrar</button>
+        </nav>
+    </header>
 </body>
 </html>
